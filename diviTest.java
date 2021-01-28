35acd515ff6f68eef9ac411170c3cdf916cb0bdd
zhangruiqiang <zrqtxdy@163.com>
2021-01-28 23:06:17 +0800

    测试除法是否正常

---
+++ diviTest.java
@@ -0,0 +1,10 @@
+package day_01;
+
+public class diviTest {
+    public static void main(String[] args) {
+        Fraction f1=new Fraction(1,3);
+        Fraction f2=new Fraction(1,6);
+        Fraction f3=f1.divi(f2);
+        f3.print();
+    }
+}
