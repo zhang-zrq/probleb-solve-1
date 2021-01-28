819185337bfed25d00c6f8527c915e569685c2aa
zhangruiqiang <zrqtxdy@163.com>
2021-01-28 23:04:45 +0800

    测试加法是否正常

---
+++ addTest.java
@@ -0,0 +1,9 @@
+package day_01;
+public class addTest {
+    public static void main(String[] args) {
+    Fraction f1=new Fraction(1,3);
+    Fraction f2=new Fraction(1,6);
+    Fraction f3=f1.add(f2);
+    f3.print();
+    }
+}
