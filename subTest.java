a7a3aa0836597a7c20e377310e4890eb9ee08631
zhangruiqiang <zrqtxdy@163.com>
2021-01-28 23:09:00 +0800

    测试减法是否正常

---
+++ subTest.java
@@ -0,0 +1,10 @@
+package day_01;
+
+public class subTest {
+    public static void main(String[] args) {
+        Fraction f1=new Fraction(1,3);
+        Fraction f2=new Fraction(1,3);
+        Fraction f3=f1.sub(f2);
+        f3.print();
+    }
+}
